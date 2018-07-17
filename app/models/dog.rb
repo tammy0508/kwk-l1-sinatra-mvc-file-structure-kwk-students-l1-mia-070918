5b46f8a7cee797ea66c7d20bf3d4dce53c135fd0
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-17 18:35:01 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+
+  def initialize(name, breed, age)
+    @name=name
+    @breed=breed
+    @age=age
+    @@all << name
+  end
+
+  def self.all
+    @@all
+  end
+
+end
+
+# rudy = Dog.new("rudy", "german", "2")
+# bob = Dog.new("bob", "pitbull", "3")
+#
+# puts Dog.all
