cd28ead4d7cf55931c8d39041ff9bb84e7ba7b5e
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-17 18:34:47 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,9 +1,9 @@
 class Dog
   # Replace with Dog clas 
-  def initialize (breed, color_fur, color_eyes)
-    @breed=breed
-    @color_fur=color_fur
-    @color_eyes=color_eyes
-  end 
-attr_accessor :breed, :color_fur, :color_eyes
+#   def initialize (breed, color_fur, color_eyes)
+#     @breed=breed
+#     @color_fur=color_fur
+#     @color_eyes=color_eyes
+#   end 
+# attr_accessor :breed, :color_fur, :color_eyes
 end
