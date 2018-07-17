f9f4d23764e3fe399e0f0b6cc55d91113c5d9247
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-17 18:18:52 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,19 @@
 class Dog
-  # Replace with Dog clas
+  # Replace with Dog class
+  all=[]
+  def self.all
+
+  end
+
   def initialize (name, breed, age)
     @name=name
+    all << name
     @breed=breed
     @age=age
   end
+
 attr_accessor :name, :breed, :age
 end
+
+first_dog= Dog.new (lucy ,golden retriver, 13 )
+puts all.inspect 
